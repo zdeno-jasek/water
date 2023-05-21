a412545b788b8085bf51165f3f66492641160239
zdeno-jasek <32713775+zdeno-jasek@users.noreply.github.com>
2023-05-21 09:28:39 +0200

    init

---
+++ src/test/java/sk/zdeno/water/WaterApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.zdeno.water;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
